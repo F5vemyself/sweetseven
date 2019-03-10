f744fef199bdfa1bb4b01a4ee241cfd7ef039760
F5vemyself <592578839@qq.com>
2019-03-10 20:52:09 +0800

    commit the last task files

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
- 
- 
+
+
 // 创建服务器
 http.createServer( function (request, response) 
-{  
+{
    // 解析请求，包括文件名
    var pathname = url.parse(request.url).pathname;
    //parse表示分割字符串
    //url.parse(request.url)表示分割请求的url的pathname
-   
+
    // 输出请求的文件名
    console.log("Request for " + pathname + " received.");
-   
+
    // 从文件系统中读取请求的文件内容
    fs.readFile(pathname.substr(1), function (err, data) 
    {
@@ -43,26 +43,14 @@ http.createServer( function (request, response)
                 contentType = 'text/html';
             }  
             response.writeHead(200, {'Content-Type': contentType});    
-         
+
             // 响应文件内容
-            response.write(data.toString());        
+            response.write(data);        
         }
         //  发送响应数据
          response.end();
 
     }); 
- 
-//       fs.readFile('./public/**/*.jpg', function (err, data) {
-//         if (err) {
-//            console.log(err);
-//            response.writeHead(404, {'Content-Type': 'image/jpg'});
-//         }
-//         else {
-//             response.writeHead(200, {'Content-Type': 'image/jpg'});    
-//         }
-    
-//    });  
-//    response.end();
  
 }).listen(8080);
  
